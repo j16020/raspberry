391b328f870cd432f584fd6149f00bf56f6bfd69
j16020 <j16020@sangi.jp>
2020-02-07 16:40:12 +0900

    add to shell

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python3
+pip3 install untangle
+pip3 install zeroconf
+sudo apt-get install bluetooth
+sudo apt-get install bluez
+sudo apt-get install python-bluez
+sudo apt-get install python-pip libglib2.0-dev
+sudo pip install bluepy
+sudo apt-get update
+git clone https://github.com/amymcgovern/pyparrot
